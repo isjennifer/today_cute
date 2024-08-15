870a03e4d4bb0ced66ed2443af47137b40ab3c32
isjennifer <duensdl@gmail.com>
2024-08-16 00:07:57 +0900

    업로드 페이지 구현 완료

--- lib/upload.dart
+++ lib/upload.dart
@@ -1,202 +1,321 @@
-import 'dart:io';
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+import 'package:google_fonts/google_fonts.dart';
 import 'package:image_picker/image_picker.dart';
+import 'dart:io';
+import 'package:permission_handler/permission_handler.dart';
 
 class UploadPage extends StatefulWidget {
   const UploadPage({super.key});
 
   @override
-  State<UploadPage> createState() => _UploadPageState();
+  _UploadPageState createState() => _UploadPageState();
 }
 
 class _UploadPageState extends State<UploadPage> {
-  // Image Picker 인스턴스 생성
-  final ImagePicker picker = ImagePicker();
+  List<File> _images = [];
+  File? _video;
+  final picker = ImagePicker();
 
-  // 카메라 또는 갤러리의 이미지를 저장할 변수
-  XFile? _imageFile;
+  Future getImages() async {
+    final pickedFiles = await picker.pickMultiImage();
 
-  // 이미지를 가져오는 함수
-  Future<void> getImage(ImageSource imageSource) async {
-    try {
-      // 카메라 또는 갤러리의 이미지
-      final XFile? imageFile = await picker.pickImage(
-          source: imageSource, maxHeight: 300, maxWidth: 300);
+    if (pickedFiles != null && pickedFiles.isNotEmpty) {
+      setState(() {
+        _images = pickedFiles.map((file) => File(file.path)).toList();
+      });
 
-      if (imageFile != null) {
-        // 이미지를 화면에 출력하기 위해 앱의 상태 변경
-        setState(() {
-          _imageFile = imageFile;
-        });
-      }
-    } catch (e) {
-      print("디버깅용 이미지 호출 에러 : $e");
+      Navigator.push(
+        context,
+        MaterialPageRoute(
+          builder: (context) => PostCreationPage(images: _images),
+        ),
+      );
+    } else {
+      // 사진 선택이 취소되었을 경우 (pickedFiles가 null이거나 비어 있을 때)
+      // 네비게이션을 작동시키지 않음.
     }
+  }
+
+  Future getVideo(ImageSource source) async {
+    final pickedFile = await picker.pickVideo(source: source);
+
+    setState(() {
+      if (pickedFile != null) {
+        _video = File(pickedFile.path);
+      }
+    });
   }
 
   @override
   Widget build(BuildContext context) {
-    return SafeArea(
-      child: Container(
-        color: Colors.white,
-        child: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              SizedBox(
-                width: 300,
-                height: 300,
-                child: _buildPhotoArea(),
+    return Scaffold(
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: <Widget>[
+            Expanded(
+              child: Container(
+                width: double.infinity,
+                height: 100,
+                margin: EdgeInsets.symmetric(horizontal: 10, vertical: 5),
+                child: ElevatedButton(
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: Color(0XFFCCFFFF), // 버튼의 배경색 (예: 파란색)
+                    foregroundColor:
+                        Color.fromARGB(255, 21, 120, 120), // 버튼의 텍스트 색상 (예: 흰색)
+                    // 버튼 내부의 패딩
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(50), // 버튼의 둥근 테두리
+                    ),
+                  ),
+                  child: Text('갤러리에서 이미지 선택'),
+                  onPressed: getImages,
+                ),
               ),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  // 카메라 선택 버튼
-                  ElevatedButton(
-                    onPressed: () {
-                      getImage(ImageSource.camera);
-                    },
-                    child: const Text("카메라"),
+            ),
+            Expanded(
+              child: Container(
+                width: double.infinity,
+                height: 100,
+                margin: EdgeInsets.symmetric(horizontal: 10, vertical: 5),
+                child: ElevatedButton(
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: Color(0XFFCCFFFF), // 버튼의 배경색 (예: 파란색)
+                    foregroundColor:
+                        Color.fromARGB(255, 21, 120, 120), // 버튼의 텍스트 색상 (예: 흰색)
+                    // 버튼 내부의 패딩
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(50), // 버튼의 둥근 테두리
+                    ),
                   ),
-                  const Padding(padding: EdgeInsets.all(10)),
-                  // 앨범 선택 버튼
-                  ElevatedButton(
-                    onPressed: () {
-                      getImage(ImageSource.gallery);
-                    },
-                    child: const Text("앨범"),
+                  child: Text('갤러리에서 동영상 선택'),
+                  onPressed: () => getVideo(ImageSource.gallery),
+                ),
+              ),
+            ),
+            Expanded(
+              child: Container(
+                width: double.infinity,
+                height: 100,
+                margin: EdgeInsets.symmetric(horizontal: 10, vertical: 5),
+                child: ElevatedButton(
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: Color(0XFFCCFFFF), // 버튼의 배경색 (예: 파란색)
+                    foregroundColor:
+                        Color.fromARGB(255, 21, 120, 120), // 버튼의 텍스트 색상 (예: 흰색)
+                    // 버튼 내부의 패딩
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(50), // 버튼의 둥근 테두리
+                    ),
                   ),
-                ],
-              )
-            ],
-          ),
+                  child: Text('카메라로 사진 촬영'),
+                  onPressed: () async {
+                    final pickedFile =
+                        await picker.pickImage(source: ImageSource.camera);
+                    if (pickedFile != null) {
+                      setState(() {
+                        _images.add(File(pickedFile.path));
+                      });
+                    }
+                  },
+                ),
+              ),
+            ),
+            Expanded(
+              child: Container(
+                width: double.infinity,
+                height: 100,
+                margin: EdgeInsets.symmetric(horizontal: 10, vertical: 5),
+                child: ElevatedButton(
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: Color(0XFFCCFFFF), // 버튼의 배경색 (예: 파란색)
+                    foregroundColor:
+                        Color.fromARGB(255, 21, 120, 120), // 버튼의 텍스트 색상 (예: 흰색)
+                    // 버튼 내부의 패딩
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(50), // 버튼의 둥근 테두리
+                    ),
+                  ),
+                  child: Text('카메라로 동영상 촬영'),
+                  onPressed: () => getVideo(ImageSource.camera),
+                ),
+              ),
+            ),
+          ],
         ),
       ),
     );
   }
-
-  Widget _buildPhotoArea() {
-    // 불러온 이미지가 있는지 없는지 확인
-    return _imageFile != null
-        // 불러온 이미지가 있으면 출력
-        ? Center(
-            child: Image.file(
-              File(_imageFile!.path),
-            ),
-          )
-        // 불러온 이미지가 없으면 텍스트 출력
-        : const Center(
-            child: Text("불러온 이미지가 없습니다."),
-          );
-  }
 }
 
+class PostCreationPage extends StatefulWidget {
+  final List<File> images;
 
-// import 'package:flutter/material.dart';
-// import 'package:flutter/widgets.dart';
-// import 'package:google_fonts/google_fonts.dart';
-// import 'package:image_picker/image_picker.dart';
-// import 'dart:io';
-// import 'package:permission_handler/permission_handler.dart';
+  const PostCreationPage({Key? key, required this.images}) : super(key: key);
 
-// class UploadPage extends StatefulWidget {
-//   const UploadPage({super.key});
+  @override
+  _PostCreationPageState createState() => _PostCreationPageState();
+}
 
-//   @override
-//   _UploadPageState createState() => _UploadPageState();
-// }
+class _PostCreationPageState extends State<PostCreationPage> {
+  final TextEditingController _titleController = TextEditingController();
+  final TextEditingController _contentController = TextEditingController();
+  final TextEditingController _tagsController = TextEditingController();
+  List<File> _images = [];
 
-// class _UploadPageState extends State<UploadPage> {
-//   File? _image;
-//   File? _video;
-//   final picker = ImagePicker();
+  @override
+  void initState() {
+    super.initState();
+    _images = widget.images;
+  }
 
-//   Future getImage(ImageSource source) async {
-//     // 갤러리 권한 확인
-//     if (source == ImageSource.gallery) {
-//       var status = await Permission.photos.status;
-//       if (!status.isGranted) {
-//         status = await Permission.photos.request();
-//         if (!status.isGranted) {
-//           // 사용자가 권한을 거부한 경우
-//           ScaffoldMessenger.of(context).showSnackBar(
-//             SnackBar(content: Text('갤러리 접근 권한이 필요합니다.')),
-//           );
-//           return;
-//         }
-//       }
-//     }
+  Future addMoreImages() async {
+    final picker = ImagePicker();
+    final pickedFiles = await picker.pickMultiImage();
 
-//     try {
-//       final pickedFile = await picker.pickImage(source: source);
+    if (pickedFiles != null) {
+      setState(() {
+        _images.addAll(pickedFiles.map((file) => File(file.path)).toList());
+      });
+    }
+  }
 
-//       setState(() {
-//         if (pickedFile != null) {
-//           _image = File(pickedFile.path);
-//         }
-//       });
-//     } catch (e) {
-//       print('이미지 선택 중 오류 발생: $e');
-//       ScaffoldMessenger.of(context).showSnackBar(
-//         SnackBar(content: Text('이미지를 선택하는 중 오류가 발생했습니다.')),
-//       );
-//     }
-//   }
+  void removeImage(int index) {
+    setState(() {
+      _images.removeAt(index);
+    });
+  }
 
-//   Future getVideo(ImageSource source) async {
-//     final pickedFile = await picker.pickVideo(source: source);
-
-//     setState(() {
-//       if (pickedFile != null) {
-//         _video = File(pickedFile.path);
-//       }
-//     });
-//   }
-
-//   @override
-//   Widget build(BuildContext context) {
-//     return Scaffold(
-//       body: Center(
-//         child: Column(
-//           mainAxisAlignment: MainAxisAlignment.center,
-//           children: <Widget>[
-//             _image != null
-//                 ? Image.file(_image!, height: 200)
-//                 : Text('선택된 이미지 없음'),
-//             SizedBox(height: 20),
-//             _video != null
-//                 ? Text('비디오 선택됨: ${_video!.path}')
-//                 : Text('선택된 비디오 없음'),
-//             SizedBox(height: 20),
-//             ElevatedButton(
-//               child: Text('갤러리에서 사진 선택'),
-//               onPressed: () => getImage(ImageSource.gallery),
-//             ),
-//             ElevatedButton(
-//               child: Text('카메라로 사진 촬영'),
-//               onPressed: () => getImage(ImageSource.camera),
-//             ),
-//             ElevatedButton(
-//               child: Text('갤러리에서 동영상 선택'),
-//               onPressed: () => getVideo(ImageSource.gallery),
-//             ),
-//             ElevatedButton(
-//               child: Text('카메라로 동영상 촬영'),
-//               onPressed: () => getVideo(ImageSource.camera),
-//             ),
-//             ElevatedButton(
-//               child: Text('업로드'),
-//               onPressed: () {
-//                 // 여기에 실제 업로드 로직을 구현합니다.
-//                 // 예: 서버로 _image 또는 _video 파일 전송
-//                 ScaffoldMessenger.of(context).showSnackBar(
-//                   SnackBar(content: Text('업로드 기능은 아직 구현되지 않았습니다.')),
-//                 );
-//               },
-//             ),
-//           ],
-//         ),
-//       ),
-//     );
-//   }
-// }
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: Text('게시물 작성'),
+        ),
+        body: SingleChildScrollView(
+          child: Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: <Widget>[
+                TextField(
+                  controller: _titleController,
+                  decoration: InputDecoration(
+                    labelText: '제목',
+                    border: OutlineInputBorder(),
+                  ),
+                ),
+                SizedBox(height: 10),
+                TextField(
+                  controller: _contentController,
+                  maxLines: 5,
+                  decoration: InputDecoration(
+                    labelText: '내용',
+                    border: OutlineInputBorder(),
+                  ),
+                ),
+                SizedBox(height: 10),
+                TextField(
+                  controller: _tagsController,
+                  decoration: InputDecoration(
+                    labelText: '태그 (#으로 구분하여 작성)',
+                    border: OutlineInputBorder(),
+                  ),
+                ),
+                SizedBox(height: 10),
+                GridView.builder(
+                  shrinkWrap: true,
+                  physics: NeverScrollableScrollPhysics(),
+                  gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                    crossAxisCount: 3,
+                    crossAxisSpacing: 4.0,
+                    mainAxisSpacing: 4.0,
+                  ),
+                  itemCount: _images.length,
+                  itemBuilder: (context, index) {
+                    return Stack(
+                      children: [
+                        Container(
+                          decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(8.0),
+                            image: DecorationImage(
+                              image: FileImage(_images[index]),
+                              fit: BoxFit.cover,
+                            ),
+                          ),
+                          child: Container(
+                            decoration: BoxDecoration(
+                              borderRadius: BorderRadius.circular(8.0),
+                              color: Colors.black.withOpacity(0.2),
+                            ),
+                          ),
+                        ),
+                        Positioned(
+                          right: 0,
+                          child: IconButton(
+                            icon: Icon(Icons.cancel, color: Colors.red),
+                            onPressed: () => removeImage(index),
+                          ),
+                        ),
+                      ],
+                    );
+                  },
+                ),
+                TextButton(
+                  style: ElevatedButton.styleFrom(
+                    minimumSize: Size(200, 50), // 버튼의 최소 크기 지정 (가로 150, 세로 50)
+                    backgroundColor: Colors.white, // 버튼의 배경색 (예: 파란색)
+                    foregroundColor: Color.fromARGB(255, 21, 120, 120),
+                    // 버튼의 텍스트 색상 (예: 흰색)
+                    padding: EdgeInsets.symmetric(
+                        horizontal: 20, vertical: 10), // 버튼 내부의 패딩
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(50), // 버튼의 둥근 테두리
+                    ),
+                  ),
+                  onPressed: addMoreImages,
+                  child: Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Icon(
+                          Icons.add,
+                          size: 18,
+                        ),
+                        Text('이미지 추가')
+                      ]),
+                ),
+                SizedBox(height: 20),
+              ],
+            ),
+          ),
+        ),
+        bottomNavigationBar: BottomAppBar(
+            child: SizedBox(
+          width: double.infinity, // 가로 크기를 화면의 너비로 설정
+          child: ElevatedButton(
+            style: ElevatedButton.styleFrom(
+              backgroundColor: Color(0XFFCCFFFF), // 버튼의 배경색 (예: 파란색)
+              foregroundColor:
+                  Color.fromARGB(255, 21, 120, 120), // 버튼의 텍스트 색상 (예: 흰색)
+              padding: EdgeInsets.symmetric(
+                  horizontal: 20, vertical: 15), // 버튼 내부의 패딩
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(50), // 버튼의 둥근 테두리
+              ),
+            ),
+            child: Text(
+              '게시물 업로드',
+              style: TextStyle(fontSize: 18),
+            ),
+            onPressed: () {
+              // 업로드 로직 추가
+              ScaffoldMessenger.of(context).showSnackBar(
+                SnackBar(content: Text('게시물이 업로드되었습니다.')),
+              );
+              Navigator.pop(context);
+            },
+          ),
+        )));
+  }
+}
