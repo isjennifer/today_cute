8bdd09dfca15e48d087539889e30f82d1593d6c6
isjennifer <duensdl@gmail.com>
2024-08-21 22:57:05 +0900

    동영상 업로드 구현

--- lib/home.dart
+++ lib/home.dart
@@ -2,6 +2,10 @@ import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:smooth_page_indicator/smooth_page_indicator.dart';
+import 'package:video_player/video_player.dart';
+import 'package:flutter_ffmpeg/flutter_ffmpeg.dart';
+import 'package:path_provider/path_provider.dart';
+import 'dart:io';
 
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
@@ -13,11 +17,8 @@ class HomePage extends StatelessWidget {
         color: Color(0XFFFFFFFDE),
         child: ListView(
           children: [
-            HomeBody(),
-            HomeBody(),
-            HomeBody(),
-            HomeBody(),
-            HomeBody(),
+            ImageBody(),
+            VideoBody(),
           ],
         ),
       ),
@@ -112,14 +113,14 @@ class _ExpandableTextState extends State<ExpandableText> {
   }
 }
 
-class HomeBody extends StatefulWidget {
-  const HomeBody({super.key});
+class ImageBody extends StatefulWidget {
+  const ImageBody({super.key});
 
   @override
-  _HomeBodyState createState() => _HomeBodyState();
+  _ImageBodyState createState() => _ImageBodyState();
 }
 
-class _HomeBodyState extends State<HomeBody> {
+class _ImageBodyState extends State<ImageBody> {
   final PageController _pageController = PageController();
 
   @override
@@ -321,5 +322,211 @@ class _HomeBodyState extends State<HomeBody> {
             ),
           ),
         ]));
+  }
+}
+
+class VideoBody extends StatefulWidget {
+  const VideoBody({super.key});
+
+  @override
+  _VideoBodyState createState() => _VideoBodyState();
+}
+
+class _VideoBodyState extends State<VideoBody> {
+  late VideoPlayerController _videoController;
+
+  @override
+  void initState() {
+    super.initState();
+    _videoController = VideoPlayerController.asset('assets/sample_video.mp4')
+      ..initialize().then((_) {
+        setState(() {}); // 비디오 초기화 후 화면을 다시 그립니다.
+        _videoController.setLooping(true);
+        _videoController.play(); // 비디오 자동 재생
+      }).catchError((error) {
+        print('Error initializing video player: $error');
+      });
+  }
+
+  @override
+  void dispose() {
+    _videoController.dispose(); // 메모리 누수를 방지하기 위해 컨트롤러를 해제합니다.
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: double.infinity,
+      color: Colors.white,
+      margin: EdgeInsets.fromLTRB(0, 30, 0, 20),
+      child: Stack(
+        children: [
+          Column(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              Container(
+                padding: const EdgeInsets.only(
+                    top: 40, left: 20, right: 20, bottom: 5),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Flexible(
+                      child: Text(
+                        '우리집 강아지 자랑좀 할게여라라라라라라ㅏ라랄라ㅏㄹㄹㄹ',
+                        style: TextStyle(
+                          fontSize: 18, // 텍스트 크기 설정
+                        ),
+                        overflow: TextOverflow.ellipsis, // 길어지면 ...으로 표시
+                        maxLines: 1, // 한 줄로 제한
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              Container(
+                padding: const EdgeInsets.symmetric(vertical: 10.0),
+                width: double.infinity,
+                height: 340,
+                child: ClipRect(
+                  child: _videoController.value.isInitialized
+                      ? AspectRatio(
+                          aspectRatio: _videoController.value.aspectRatio,
+                          child: VideoPlayer(_videoController),
+                        )
+                      : Center(child: CircularProgressIndicator()),
+                ),
+              ),
+              Container(
+                padding: const EdgeInsets.symmetric(
+                    vertical: 10.0, horizontal: 20.0),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Row(
+                      children: [
+                        Icon(Icons.person),
+                        SizedBox(width: 8),
+                        Text('아이디'),
+                      ],
+                    ),
+                    Row(
+                      children: [
+                        Icon(Icons.favorite_border),
+                        SizedBox(width: 8),
+                        Icon(Icons.bookmark_border),
+                      ],
+                    ),
+                  ],
+                ),
+              ),
+              Container(
+                padding:
+                    const EdgeInsets.symmetric(horizontal: 20.0, vertical: 10),
+                child: ExpandableText(
+                  text:
+                      '우리집 강아지 너무 귀엽지ddddd 않나요? 이 강아지는 저와 함께 산책도 하고 놀아주기도 하고 정말 즐거운 시간을 보내고 있어요. 너무 사랑스럽고 귀여워서 하루하루가 정말 행복해요. 강아지가 제 인생에 얼마나 큰 기쁨을 주는지 말로 표현할 수 없어요. 정말 너무너무 귀엽답니다!',
+                ),
+              ),
+              Container(
+                padding: const EdgeInsets.only(
+                    left: 25, top: 5, right: 25, bottom: 25),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Row(
+                      children: [
+                        Text(
+                          '2024년 10월 20일',
+                          style: TextStyle(color: Colors.grey),
+                        ),
+                      ],
+                    ),
+                    Container(
+                      width: 250,
+                      child: SingleChildScrollView(
+                        scrollDirection: Axis.horizontal,
+                        child: Row(
+                          children: [
+                            Container(
+                              padding: EdgeInsets.symmetric(
+                                  horizontal: 12, vertical: 4),
+                              decoration: BoxDecoration(
+                                color: Color(0XFFFFF99CC),
+                                borderRadius: BorderRadius.circular(14),
+                              ),
+                              child: Text(
+                                '#고양이',
+                                style: TextStyle(
+                                  color: Colors.black,
+                                ),
+                              ),
+                            ),
+                            Container(
+                              padding: EdgeInsets.symmetric(
+                                  horizontal: 12, vertical: 4),
+                              decoration: BoxDecoration(
+                                color: Color(0XFFFFFFF66),
+                                borderRadius: BorderRadius.circular(14),
+                              ),
+                              child: Text(
+                                '#귀욤짤',
+                                style: TextStyle(
+                                  color: Colors.black,
+                                ),
+                              ),
+                            ),
+                            Container(
+                              padding: EdgeInsets.symmetric(
+                                  horizontal: 12, vertical: 4),
+                              decoration: BoxDecoration(
+                                color: Color(0XFFFCCFFFF),
+                                borderRadius: BorderRadius.circular(14),
+                              ),
+                              child: Text(
+                                '#강아지',
+                                style: TextStyle(
+                                  color: Colors.black,
+                                ),
+                              ),
+                            ),
+                            Container(
+                              padding: EdgeInsets.symmetric(
+                                  horizontal: 12, vertical: 4),
+                              decoration: BoxDecoration(
+                                color: Color(0XFFFCCFFFF),
+                                borderRadius: BorderRadius.circular(14),
+                              ),
+                              child: Text(
+                                '#강아지',
+                                style: TextStyle(
+                                  color: Colors.black,
+                                ),
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          ),
+          Transform.translate(
+            offset: Offset(0, -20),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Image.asset(
+                  'assets/character_color.png',
+                  width: 150,
+                ),
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
--- lib/upload.dart
+++ lib/upload.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:image_picker/image_picker.dart';
+import 'package:video_player/video_player.dart';
 import 'dart:io';
 import 'package:permission_handler/permission_handler.dart';
 
@@ -40,11 +41,21 @@ class _UploadPageState extends State<UploadPage> {
   Future getVideo(ImageSource source) async {
     final pickedFile = await picker.pickVideo(source: source);
 
-    setState(() {
-      if (pickedFile != null) {
+    if (pickedFile != null) {
+      setState(() {
         _video = File(pickedFile.path);
-      }
-    });
+      });
+
+      Navigator.push(
+        context,
+        MaterialPageRoute(
+          builder: (context) => PostCreationPage(
+            images: _images, // images 인자를 전달합니다.
+            video: _video, // 선택적으로 video 인자를 전달합니다.
+          ),
+        ),
+      );
+    }
   }
 
   @override
@@ -151,8 +162,10 @@ class _UploadPageState extends State<UploadPage> {
 
 class PostCreationPage extends StatefulWidget {
   final List<File> images;
+  final File? video;
 
-  const PostCreationPage({Key? key, required this.images}) : super(key: key);
+  const PostCreationPage({Key? key, required this.images, this.video})
+      : super(key: key);
 
   @override
   _PostCreationPageState createState() => _PostCreationPageState();
@@ -163,14 +176,32 @@ class _PostCreationPageState extends State<PostCreationPage> {
   final TextEditingController _contentController = TextEditingController();
   final TextEditingController _tagsController = TextEditingController();
   List<File> _images = [];
+  File? _video;
+  VideoPlayerController? _videoController;
+  Future<void>? _initializeVideoPlayerFuture;
 
   @override
   void initState() {
     super.initState();
     _images = widget.images;
+    _video = widget.video;
+
+    if (_video != null) {
+      _videoController = VideoPlayerController.file(_video!)..setLooping(true);
+      _initializeVideoPlayerFuture = _videoController!.initialize();
+    }
   }
 
-  Future addMoreImages() async {
+  @override
+  void dispose() {
+    _titleController.dispose();
+    _contentController.dispose();
+    _tagsController.dispose();
+    _videoController?.dispose();
+    super.dispose();
+  }
+
+  Future<void> addMoreImages() async {
     final picker = ImagePicker();
     final pickedFiles = await picker.pickMultiImage();
 
@@ -187,121 +218,157 @@ class _PostCreationPageState extends State<PostCreationPage> {
     });
   }
 
+  void _toggleVideoPlayback() {
+    setState(() {
+      if (_videoController!.value.isPlaying) {
+        _videoController!.pause();
+      } else {
+        _videoController!.play();
+      }
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          title: Text('게시물 작성'),
-        ),
-        body: SingleChildScrollView(
-          child: Padding(
-            padding: const EdgeInsets.all(16.0),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: <Widget>[
-                TextField(
-                  controller: _titleController,
-                  decoration: InputDecoration(
-                    labelText: '제목',
-                    border: OutlineInputBorder(),
-                  ),
+      appBar: AppBar(
+        title: Text('게시물 작성'),
+      ),
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: <Widget>[
+              TextField(
+                controller: _titleController,
+                decoration: InputDecoration(
+                  labelText: '제목',
+                  border: OutlineInputBorder(),
                 ),
-                SizedBox(height: 10),
-                TextField(
-                  controller: _contentController,
-                  maxLines: 5,
-                  decoration: InputDecoration(
-                    labelText: '내용',
-                    border: OutlineInputBorder(),
-                  ),
+              ),
+              SizedBox(height: 10),
+              TextField(
+                controller: _contentController,
+                maxLines: 5,
+                decoration: InputDecoration(
+                  labelText: '내용',
+                  border: OutlineInputBorder(),
                 ),
-                SizedBox(height: 10),
-                TextField(
-                  controller: _tagsController,
-                  decoration: InputDecoration(
-                    labelText: '태그 (#으로 구분하여 작성)',
-                    border: OutlineInputBorder(),
-                  ),
+              ),
+              SizedBox(height: 10),
+              TextField(
+                controller: _tagsController,
+                decoration: InputDecoration(
+                  labelText: '태그 (#으로 구분하여 작성)',
+                  border: OutlineInputBorder(),
                 ),
-                SizedBox(height: 10),
-                GridView.builder(
-                  shrinkWrap: true,
-                  physics: NeverScrollableScrollPhysics(),
-                  gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-                    crossAxisCount: 3,
-                    crossAxisSpacing: 4.0,
-                    mainAxisSpacing: 4.0,
-                  ),
-                  itemCount: _images.length,
-                  itemBuilder: (context, index) {
-                    return Stack(
-                      children: [
-                        Container(
-                          decoration: BoxDecoration(
-                            borderRadius: BorderRadius.circular(8.0),
-                            image: DecorationImage(
-                              image: FileImage(_images[index]),
-                              fit: BoxFit.cover,
+              ),
+              SizedBox(height: 10),
+              if (_video == null) // 동영상이 없는 경우에만 이미지 그리드 및 추가 버튼 표시
+                Column(
+                  children: [
+                    GridView.builder(
+                      shrinkWrap: true,
+                      physics: NeverScrollableScrollPhysics(),
+                      gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                        crossAxisCount: 3,
+                        crossAxisSpacing: 4.0,
+                        mainAxisSpacing: 4.0,
+                      ),
+                      itemCount: _images.length,
+                      itemBuilder: (context, index) {
+                        return Stack(
+                          children: [
+                            Container(
+                              decoration: BoxDecoration(
+                                borderRadius: BorderRadius.circular(8.0),
+                                image: DecorationImage(
+                                  image: FileImage(_images[index]),
+                                  fit: BoxFit.cover,
+                                ),
+                              ),
+                              child: Container(
+                                decoration: BoxDecoration(
+                                  borderRadius: BorderRadius.circular(8.0),
+                                  color: Colors.black.withOpacity(0.2),
+                                ),
+                              ),
                             ),
-                          ),
-                          child: Container(
-                            decoration: BoxDecoration(
-                              borderRadius: BorderRadius.circular(8.0),
-                              color: Colors.black.withOpacity(0.2),
+                            Positioned(
+                              right: 0,
+                              child: IconButton(
+                                icon: Icon(Icons.cancel, color: Colors.red),
+                                onPressed: () => removeImage(index),
+                              ),
                             ),
-                          ),
-                        ),
-                        Positioned(
-                          right: 0,
-                          child: IconButton(
-                            icon: Icon(Icons.cancel, color: Colors.red),
-                            onPressed: () => removeImage(index),
-                          ),
-                        ),
-                      ],
-                    );
-                  },
-                ),
-                TextButton(
-                  style: ElevatedButton.styleFrom(
-                    minimumSize: Size(200, 50), // 버튼의 최소 크기 지정 (가로 150, 세로 50)
-                    backgroundColor: Colors.white, // 버튼의 배경색 (예: 파란색)
-                    foregroundColor: Color.fromARGB(255, 21, 120, 120),
-                    // 버튼의 텍스트 색상 (예: 흰색)
-                    padding: EdgeInsets.symmetric(
-                        horizontal: 20, vertical: 10), // 버튼 내부의 패딩
-                    shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(50), // 버튼의 둥근 테두리
+                          ],
+                        );
+                      },
                     ),
-                  ),
-                  onPressed: addMoreImages,
-                  child: Row(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: [
-                        Icon(
-                          Icons.add,
-                          size: 18,
+                    SizedBox(height: 10),
+                    TextButton(
+                      style: ElevatedButton.styleFrom(
+                        minimumSize: Size(200, 50),
+                        backgroundColor: Colors.white,
+                        foregroundColor: Color.fromARGB(255, 21, 120, 120),
+                        padding:
+                            EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(50),
                         ),
-                        Text('이미지 추가')
-                      ]),
+                      ),
+                      onPressed: addMoreImages,
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [Icon(Icons.add, size: 18), Text('이미지 추가')],
+                      ),
+                    ),
+                  ],
                 ),
-                SizedBox(height: 20),
-              ],
-            ),
+              if (_video != null) // 동영상이 있는 경우 동영상 미리보기 및 클릭 시 재생/일시정지
+                Column(
+                  children: [
+                    SizedBox(height: 10),
+                    Text(
+                      '동영상 미리보기 (터치하여 재생) :',
+                      style: TextStyle(
+                          fontWeight: FontWeight.bold, color: Colors.grey),
+                    ),
+                    SizedBox(height: 10),
+                    FutureBuilder(
+                      future: _initializeVideoPlayerFuture,
+                      builder: (context, snapshot) {
+                        if (snapshot.connectionState == ConnectionState.done) {
+                          return GestureDetector(
+                            onTap: _toggleVideoPlayback, // 동영상 클릭 시 재생/일시정지
+                            child: AspectRatio(
+                              aspectRatio: _videoController!.value.aspectRatio,
+                              child: VideoPlayer(_videoController!),
+                            ),
+                          );
+                        } else {
+                          return Center(child: CircularProgressIndicator());
+                        }
+                      },
+                    ),
+                  ],
+                ),
+              SizedBox(height: 20),
+            ],
           ),
         ),
-        bottomNavigationBar: BottomAppBar(
-            child: SizedBox(
-          width: double.infinity, // 가로 크기를 화면의 너비로 설정
+      ),
+      bottomNavigationBar: BottomAppBar(
+        child: SizedBox(
+          width: double.infinity,
           child: ElevatedButton(
             style: ElevatedButton.styleFrom(
-              backgroundColor: Color(0XFFCCFFFF), // 버튼의 배경색 (예: 파란색)
-              foregroundColor:
-                  Color.fromARGB(255, 21, 120, 120), // 버튼의 텍스트 색상 (예: 흰색)
-              padding: EdgeInsets.symmetric(
-                  horizontal: 20, vertical: 15), // 버튼 내부의 패딩
+              backgroundColor: Color(0XFFCCFFFF),
+              foregroundColor: Color.fromARGB(255, 21, 120, 120),
+              padding: EdgeInsets.symmetric(horizontal: 20, vertical: 15),
               shape: RoundedRectangleBorder(
-                borderRadius: BorderRadius.circular(50), // 버튼의 둥근 테두리
+                borderRadius: BorderRadius.circular(50),
               ),
             ),
             child: Text(
@@ -316,6 +383,48 @@ class _PostCreationPageState extends State<PostCreationPage> {
               Navigator.pop(context);
             },
           ),
-        )));
+        ),
+      ),
+    );
+  }
+}
+
+// 동영상 재생을 위한 위젯 추가
+class VideoPlayerWidget extends StatefulWidget {
+  final File video;
+
+  const VideoPlayerWidget({Key? key, required this.video}) : super(key: key);
+
+  @override
+  _VideoPlayerWidgetState createState() => _VideoPlayerWidgetState();
+}
+
+class _VideoPlayerWidgetState extends State<VideoPlayerWidget> {
+  late VideoPlayerController _controller;
+
+  @override
+  void initState() {
+    super.initState();
+    _controller = VideoPlayerController.file(widget.video)
+      ..initialize().then((_) {
+        setState(() {});
+        _controller.play();
+      });
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return _controller.value.isInitialized
+        ? AspectRatio(
+            aspectRatio: _controller.value.aspectRatio,
+            child: VideoPlayer(_controller),
+          )
+        : Center(child: CircularProgressIndicator());
   }
 }
